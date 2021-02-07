e1065fed957b5bf5f4c20a9a786b54918a42a1b4
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-02-07 19:47:42 +0100

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Maximilian Wendel on 2021-02-07.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
